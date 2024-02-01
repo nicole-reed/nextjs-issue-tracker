a0af2f1838e4d3711b4a6d0620f7cd1cdcf14c9c
Nicole Reed <nickreed033@gmail.com>
2024-02-01 16:55:18 +0800

    implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,26 +1,30 @@
 'use client'
-import { Button, Callout, TextField } from '@radix-ui/themes'
-import SimpleMDE from "react-simplemde-editor";
+import { Button, Callout, Text, TextField } from '@radix-ui/themes'
+import SimpleMDE from 'react-simplemde-editor';
 import { useForm, Controller } from 'react-hook-form';
-import "easymde/dist/easymde.min.css";
+import 'easymde/dist/easymde.min.css';
 import axios from 'axios';
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
     const router = useRouter();
-    const { register, control, handleSubmit } = useForm<IssueForm>();
+    // react-hook form
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+        // integrate with zod
+        resolver: zodResolver(createIssueSchema)
+    });
     const [error, setError] = useState('')
 
 
     return (
         <div className='max-w-xl'>
-            {error && <Callout.Root color="red" className='mb-3'>
+            {error && <Callout.Root color='red' className='mb-3'>
                 <Callout.Text>{error}</Callout.Text>
             </Callout.Root>}
             <form className='space-y-3'
@@ -35,10 +39,13 @@ const NewIssuePage = () => {
                 <TextField.Root>
                     <TextField.Input placeholder='Title' {...register('title')} />
                 </TextField.Root>
+                {errors.title && <Text color='red' as='p'> {errors.title.message}</Text>}
                 <Controller
                     name='description'
                     control={control}
-                    render={({ field }) => <SimpleMDE placeholder="Description…" {...field} />} />
+                    render={({ field }) => <SimpleMDE placeholder='Description…' {...field} />}
+                />
+                {errors.description && <Text color='red' as='p'></Text>}
                 <Button>Submit New Issue</Button>
             </form>
         </div>
