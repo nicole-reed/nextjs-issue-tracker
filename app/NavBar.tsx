d4838caed8c4f89f8702817b1d521483269a9df5
Nicole Reed <nickreed033@gmail.com>
2024-02-01 15:29:12 +0800

    set up radix ui

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -12,25 +12,25 @@ const NavBar = () => {
 
     // to eliminate repetitive styles
     const links = [
-        { label: 'Dashboard', href: '/'},
-        { label: 'Issues', href: '/issues'},
+        { label: 'Dashboard', href: '/' },
+        { label: 'Issues', href: '/issues' },
     ]
-  return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><AiFillBug/></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => 
-            <Link 
-            key={link.href} 
-            className={classnames({
-                'text-zinc-900': link.href === currentPath,
-                'text-zinc-500': link.href !== currentPath,
-                'hover:text-zinc-800 transition-colors': true
-            })} 
-            href={link.href}>{link.label}</Link>)}
-        </ul>
-    </nav>
-  )
+    return (
+        <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+            <Link href="/"><AiFillBug /></Link>
+            <ul className='flex space-x-6'>
+                {links.map(link =>
+                    <Link
+                        key={link.href}
+                        className={classnames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors': true
+                        })}
+                        href={link.href}>{link.label}</Link>)}
+            </ul>
+        </nav>
+    )
 }
 
 export default NavBar
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-   <main>
-    <h1>Hello World</h1>
+    <main>
+      <h1>Hello World</h1>
     </main>
   )
 }
